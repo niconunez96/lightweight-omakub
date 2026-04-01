22da999009b48da5944ec8ffa1a0c19ef653377d
niconunez96 <nicolas110996@gmail.com>
2026-04-01 18:09:27 -0300

    Move lot of tools to optional

--- install/desktop/set-gnome-hotkeys.sh
+++ install/desktop/set-gnome-hotkeys.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Alt+F4 is very cumbersome
-gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Super>k']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Super>j']"
+gsettings set org.gnome.desktop.wm.keybindings cycle-windows "['<Super>l']"
+gsettings set org.gnome.desktop.wm.keybindings cycle-windows-backward "['<Super>;']"
 
 # Make it easy to maximize like you can fill left/right
 gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']"
@@ -23,16 +27,16 @@ gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
 gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
 
 # Use alt for pinned apps
-gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Alt>1']"
-gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Alt>2']"
-gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Alt>3']"
-gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Alt>4']"
-gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Alt>5']"
-gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Alt>6']"
-gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Alt>7']"
-gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Alt>8']"
-gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Alt>9']"
-
+# gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Alt>1']"
+# gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Alt>2']"
+# gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Alt>3']"
+# gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Alt>4']"
+# gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Alt>5']"
+# gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Alt>6']"
+# gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Alt>7']"
+# gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Alt>8']"
+# gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Alt>9']"
+#
 # Use super for workspaces
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
